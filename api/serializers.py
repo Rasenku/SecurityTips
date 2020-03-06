7f80c7265eb21c0cc7e8d07e6f1abfc740d5ada1
Dahveyea <dahveyea.cowan@gmail.com>
2020-03-06 03:53:16 -0800

    Failed APi so deleted it

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-
-from news.models import Article
-
-class ArticleSerializer(ModelSerializer):
-    class Meta:
-        model = Article
-        fields = '__all__'
--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from api.views import ArticleList, ArticleDetail
-
-urlpatterns = [
-    path('articles/', ArticleList.as_view(), name='article_list'),
-    path('articles/<int:pk>', ArticleDetail.as_view(), name='article_detail')
-]
