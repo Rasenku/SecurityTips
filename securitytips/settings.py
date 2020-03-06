cae802fe8eb37c6bfec26d527583d39bd10ac6f8
Dahveyea <dahveyea.cowan@gmail.com>
2020-03-06 03:04:43 -0800

    Weww

--- securitytips/settings.py
+++ securitytips/settings.py
@@ -41,11 +41,9 @@ INSTALLED_APPS = [
 
     'tips',
     'rest_framework',
-    'crispy_forms',
 
 ]
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
